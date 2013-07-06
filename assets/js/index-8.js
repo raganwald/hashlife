e62731c2ce3f14a83616f64be793dfffc67707bb
Reginald Braithwaite <reg@braythwayt.com>
2013-07-07 01:54:45 +0200

    up and down?

--- assets/js/index-8.js
+++ assets/js/index-8.js
@@ -53,8 +53,8 @@
     					.trigger( start.coords[ 0 ] > stop.coords[ 0 ] ? "swipeleft" : "swiperight" );
     			}
     			else if (
-    				Math.abs( start.coords[ 0 ] - stop.coords[ 0 ] ) < $.event.special.swipe.horizontalDistanceThreshold &&
-    				Math.abs( start.coords[ 1 ] - stop.coords[ 1 ] ) > $.event.special.swipe.verticalDistanceThreshold ) {
+    				Math.abs( start.coords[ 1 ] - stop.coords[ 1 ] ) > $.event.special.swipe.horizontalDistanceThreshold &&
+    				Math.abs( start.coords[ 0 ] - stop.coords[ 0 ] ) < $.event.special.swipe.verticalDistanceThreshold ) {
 
     				start.origin.trigger( "swipe" )
     					.trigger( start.coords[ 1 ] > stop.coords[ 1 ] ? "swipeup" : "swipedown" );
