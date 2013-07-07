c27b62e28cb93068640480de2963091133155efc
Reginald Braithwaite <reg@braythwayt.com>
2013-07-07 04:53:19 +0200

    console.log

--- assets/js/index-8.js
+++ assets/js/index-8.js
@@ -52,13 +52,21 @@
     				start.origin.trigger( "swipe" )
     					.trigger( start.coords[ 0 ] > stop.coords[ 0 ] ? "swipeleft" : "swiperight" );
     			}
-    			else if (
-    				Math.abs( start.coords[ 1 ] - stop.coords[ 1 ] ) > $.event.special.swipe.horizontalDistanceThreshold &&
-    				Math.abs( start.coords[ 0 ] - stop.coords[ 0 ] ) < $.event.special.swipe.verticalDistanceThreshold ) {
+    			else {
+      			console.log(coords);
+    			
+      			if (
+      				Math.abs( start.coords[ 1 ] - stop.coords[ 1 ] ) > $.event.special.swipe.horizontalDistanceThreshold &&
+      				Math.abs( start.coords[ 0 ] - stop.coords[ 0 ] ) < $.event.special.swipe.verticalDistanceThreshold ) {
 
-    				start.origin.trigger( "swipe" )
-    					.trigger( start.coords[ 1 ] > stop.coords[ 1 ] ? "swipeup" : "swipedown" );
+              console.log(coords);
+
+      				start.origin.trigger( "swipe" )
+      					.trigger( start.coords[ 1 ] > stop.coords[ 1 ] ? "swipeup" : "swipedown" );
+      			}
+    			
     			}
+    			
   			}
   		};
         
